c7cc9f716b488b8e4a1e44a4e24ea2559bf27587
NJEM MABULA 55 <mabulanjem55@gmail.com>
2026-05-27 12:15:49 +0300

    Fix: Default SMS - MmsReceiver na HeadlessSmsSendService

---
+++ app/src/main/java/com/njem/smartsms/receiver/MmsReceiver.kt
@@ -0,0 +1,11 @@
+package com.njem.smartsms.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MmsReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // MMS receiver - inahitajika kwa Default SMS App
+    }
+}
